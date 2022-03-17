39b7f64df8a36db1ca047f888800174ceff487c7
Nathan Nichols <nwnichols02@gmail.com>
2022-03-16 18:08:37 -0600

    styling mostly done except for transitions

--- src/App.js
+++ src/App.js
@@ -3,98 +3,173 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      
       {/* Model 3 */}
 
       <div className="model-3-img">
-      <div className="header-wrapper">
-        <header className="header">
-          <h1>
-            <a href="localhost:3000/">TESLA</a>
-          </h1>
-          <ol>
-            <button>
-              <span>Menu</span>
-            </button>
-          </ol>
-        </header>
-      </div>
+        <div className="header-wrapper">
+          <header className="header">
+            <img
+              href="localhost:3000/"
+              src="https://png.monster/wp-content/uploads/2021/03/Tesla_Inc.-Wordmark-Black-Logo.wine-6bbe293a-370x247.png.webp"
+            />
+            <nav>
+              <ul className="nav__links">
+                <li>
+                  <a href="www.tesla.com/models">Model S</a>
+                </li>
+                <li>
+                  <a href="www.tesla.com/models">Model 3</a>
+                </li>
+                <li>
+                  <a href="www.tesla.com/models">Model X</a>
+                </li>
+                <li>
+                  <a href="www.tesla.com/models">Model Y</a>
+                </li>
+                <li>
+                  <a href="www.tesla.com/models">Solar Roof</a>
+                </li>
+                <li>
+                  <a href="www.tesla.com/models">Solar Panels</a>
+                </li>
+              </ul>
+            </nav>
+            <ul className="nav__buttons">
+              <li>
+                <a className="cta" href="localhost:3000/">
+                  <button>Shop</button>
+                </a>
+              </li>
+              <li>
+                <a className="cta" href="localhost:3000/">
+                  <button>Account</button>
+                </a>
+              </li>
+              <li>
+                <a className="cta" href="localhost:3000/">
+                  <button>Menu</button>
+                </a>
+              </li>
+            </ul>
+
+            {/* <button>Shop</button>
+            <button>Account</button> */}
+          </header>
+        </div>
         <div className="title">
           <h2>Model 3</h2>
-          <p>Order Online for Touchless Delivery</p>
+          <p>
+            Order Online for <u>Touchless Delivery</u>
+          </p>
         </div>
         <div className="order-buttons">
           <a
             //fix this to become my own build your own tesla page
             href="https://www.tesla.com/model3/design#overview"
           >
-            <button>Custom Order</button>
+            <button className="button1">Custom Order</button>
           </a>
-          <button>Existing Inventory</button>
+          <button className="button2">Existing Inventory</button>
         </div>
       </div>
       {/* Model Y */}
       <div className="model-y-img">
         <div className="title">
           <h2>Model Y</h2>
-          <p>Order Online for Touchless Delivery</p>
+          <p>
+            Order Online for <u>Touchless Delivery</u>
+          </p>
         </div>
         <div className="order-buttons">
-          <button>Custom Order</button>
-          <button>Existing Inventory</button>
+          <a
+            //fix this to become my own build your own tesla page
+            href="https://www.tesla.com/model3/design#overview"
+          >
+            <button className="button1">Custom Order</button>
+          </a>
+          <button className="button2">Existing Inventory</button>
         </div>
       </div>
       {/* Model S */}
       <div className="model-s-img">
         <div className="title">
           <h2>Model S</h2>
-          <p>Order Online for Touchless Delivery</p>
+          <p>
+            Order Online for <u>Touchless Delivery</u>
+          </p>
         </div>
         <div className="order-buttons">
-          <button>Custom Order</button>
-          <button>Existing Inventory</button>
+          <a
+            //fix this to become my own build your own tesla page
+            href="https://www.tesla.com/model3/design#overview"
+          >
+            <button className="button1">Custom Order</button>
+          </a>
+          <button className="button2">Existing Inventory</button>
         </div>
       </div>
       {/* Model X */}
       <div className="model-x-img">
         <div className="title">
           <h2>Model X</h2>
-          <p>Order Online for Touchless Delivery</p>
+          <p>
+            Order Online for <u>Touchless Delivery</u>
+          </p>
         </div>
         <div className="order-buttons">
-          <button>Custom Order</button>
-          <button>Existing Inventory</button>
+          <a
+            //fix this to become my own build your own tesla page
+            href="https://www.tesla.com/model3/design#overview"
+          >
+            <button className="button1">Custom Order</button>
+          </a>
+          <button className="button2">Existing Inventory</button>
         </div>
       </div>
       {/* Solar Panels */}
       <div className="solar-panels-img">
         <div className="title">
-          <h1>Solar Panels</h1>
+          <h2>Solar Panels</h2>
           <p>Lowest Cost Solar Panels In America</p>
         </div>
-        <div className="solar-panels-order-buttons">
-          <button>Order Now</button>
-          <button>Learn More</button>
+        <div className="order-buttons">
+          <a
+            //fix this to become my own build your own tesla page
+            href="https://www.tesla.com/model3/design#overview"
+          >
+            <button className="button1">Order Now</button>
+          </a>
+          <button className="button2">Learn More</button>
         </div>
       </div>
       {/* Solar Roof */}
       <div className="solar-roof-img">
         <div className="title">
-          <h1>Solar Roof</h1>
+          <h2>Solar Roof</h2>
           <p>Produce Clean Energy From Your Roof</p>
         </div>
-        <div className="solar-roof-order-buttons">
-          <button>Order Now</button>
-          <button>Learn More</button>
+        <div className="order-buttons">
+          <a
+            //fix this to become my own build your own tesla page
+            href="https://www.tesla.com/model3/design#overview"
+          >
+            <button className="button1">Order Now</button>
+          </a>
+          <button className="button2">Learn More</button>
         </div>
       </div>
       {/* Accessories */}
       <div className="accessories-img">
         <div className="title">
-          <h1>Accessories</h1>
+          <h2>Accessories</h2>
         </div>
-        <div className="accessories-buttons">
-          <button>Shop Now</button>
+        <div className="order-buttons">
+          <a
+            //fix this to become my own build your own tesla page
+            href="https://www.tesla.com/model3/design#overview"
+          >
+            <button className="button1">Shop Now</button>
+          </a>
         </div>
       </div>
       <div className="footer">
