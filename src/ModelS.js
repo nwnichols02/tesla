382151f33017cea7910fa4ff9d99065cd6692858
Nathan Nichols <nwnichols02@gmail.com>
2022-03-18 11:42:47 -0600

    getting info for model s page

--- src/ModelS.js
+++ src/ModelS.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './ModelS.css'
 
 function ModelS() {
   return (
@@ -72,6 +73,33 @@ function ModelS() {
           <p></p>
         </div>
       </div>
+      <div className="model-s-interior-img">
+        <div className="interior">
+          <h2>Interior of the Future</h2>
+        </div>
+        
+        </div>
+        <div className="model-s-video">
+            {/* I need to implement a casserole for 5 videos left and right  */}
+            <video width={'50%'} height={'50%'} autoPlay>
+                <source src="https://tesla-cdn.thron.com/static/YMWZUY_MS-Interior-Carousel-1-Cinematic-Display-Desktop-2000_ZL9RTQ.mp4?xseo="></source>
+            </video>
+            {/* //fix the auto play to only play while you are on that screen */}
+            <h3>Cinematic Experience</h3>
+            <p>A 17” touchscreen offers 2200 x 1300 resolution, true colors and exceptional responsiveness for gaming, movies and more.</p>
+        </div>
+        <div className="image-3-offset">
+            <img/>
+            <h4>Stay Connected</h4>
+            <p>Instantly connect with multi-device Bluetooth, or fast charge devices with wireless and 36-watt USB-C charging.</p>
+            <img/>
+            <h4>Immersive Sound</h4>
+            <p>A 22-speaker, 960-watt audio system with Active Road Noise Reduction offers immersive listening and studio-grade sound quality.</p>
+            <img/>
+            <h4>Room for Everything</h4>
+            <p>With front and rear trunks and fold-flat seats you can fit your bike without taking the wheel off—and your luggage too.</p>
+        </div>
+      {/* </div> */}
     </div>
   );
 }
