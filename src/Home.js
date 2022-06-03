20eb96176478eecd62a72632801017957db18a3a
Nathan Nichols <nwnichols02@gmail.com>
2022-06-03 13:40:50 -0600

    added routing changes on custom order and existing inventory buttons

--- src/Home.js
+++ src/Home.js
@@ -20,27 +20,30 @@ class Home extends React.Component {
                 <header className="header">
                   <img
                     href="/"
+                    onClick='/'
                     src="https://png.monster/wp-content/uploads/2021/03/Tesla_Inc.-Wordmark-Black-Logo.wine-6bbe293a-370x247.png.webp"
                   />
                   <nav>
                     <ul className="nav__links">
                       <li>
-                        <a href="/model-s">Model S</a>
+                        <a href="/">Model S</a>
+                        {/* use this when model S page is completed */}
+                        {/* <a href="/model-s">Model S</a> */}
                       </li>
                       <li>
-                        <a href="www.tesla.com/models">Model 3</a>
+                        <a href="/">Model 3</a>
                       </li>
                       <li>
-                        <a href="www.tesla.com/models">Model X</a>
+                        <a href="/">Model X</a>
                       </li>
                       <li>
-                        <a href="www.tesla.com/models">Model Y</a>
+                        <a href="/">Model Y</a>
                       </li>
                       <li>
-                        <a href="www.tesla.com/models">Solar Roof</a>
+                        <a href="/">Solar Roof</a>
                       </li>
                       <li>
-                        <a href="www.tesla.com/models">Solar Panels</a>
+                        <a href="/">Solar Panels</a>
                       </li>
                     </ul>
                   </nav>
@@ -95,7 +98,7 @@ class Home extends React.Component {
                   <div className="order-buttons">
                     <a
                       //fix this to become my own build your own tesla page
-                      href="https://www.tesla.com/model3/design#overview"
+                      href="https://www.tesla.com/modelY/design#overview"
                     >
                       <button className="button1">CUSTOM ORDER</button>
                     </a>
@@ -116,7 +119,7 @@ class Home extends React.Component {
                   <div className="order-buttons">
                     <a
                       //fix this to become my own build your own tesla page
-                      href="https://www.tesla.com/model3/design#overview"
+                      href="https://www.tesla.com/modelS/design#overview"
                     >
                       <button className="button1">CUSTOM ORDER</button>
                     </a>
@@ -137,7 +140,7 @@ class Home extends React.Component {
                   <div className="order-buttons">
                     <a
                       //fix this to become my own build your own tesla page
-                      href="https://www.tesla.com/model3/design#overview"
+                      href="https://www.tesla.com/modelX/design#overview"
                     >
                       <button className="button1">CUSTOM ORDER</button>
                     </a>
@@ -156,7 +159,7 @@ class Home extends React.Component {
                   <div className="order-buttons">
                     <a
                       //fix this to become my own build your own tesla page
-                      href="https://www.tesla.com/model3/design#overview"
+                      href="https://www.tesla.com/energy/design"
                     >
                       <button className="button1">ORDER NOW</button>
                     </a>
@@ -175,7 +178,7 @@ class Home extends React.Component {
                   <div className="order-buttons">
                     <a
                       //fix this to become my own build your own tesla page
-                      href="https://www.tesla.com/model3/design#overview"
+                      href="https://www.tesla.com/energy/design"
                     >
                       <button className="button1">ORDER NOW</button>
                     </a>
@@ -193,7 +196,7 @@ class Home extends React.Component {
                   <div className="order-buttons">
                     <a
                       //fix this to become my own build your own tesla page
-                      href="https://www.tesla.com/model3/design#overview"
+                      href="https://shop.tesla.com/?tesref=true"
                     >
                       <button className="button1">SHOP NOW</button>
                     </a>
