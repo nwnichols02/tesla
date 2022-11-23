76f08f50f18d60acb9d8b278790fd52152d33a3a
Nathan Nichols <nwnichols02@gmail.com>
2022-11-22 21:03:10 -0700

    made changes to styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ModelS from "./ModelS";
 import Home from "./Home";
 import {Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App (){
   return (
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import ReactFullpage from "@fullpage/react-fullpage";
+import bootstrap from "bootstrap";
 
 class Home extends React.Component {
   render() {
@@ -20,46 +21,58 @@ class Home extends React.Component {
                 <header className="header">
                   <img
                     href="/"
-                    onClick='/'
+                    onClick="/"
                     src="https://png.monster/wp-content/uploads/2021/03/Tesla_Inc.-Wordmark-Black-Logo.wine-6bbe293a-370x247.png.webp"
                   />
                   <nav>
-                    <ul className="nav__links">
+                    <ul className="nav__links mt-3">
                       <li>
-                        <a href="/">Model S</a>
+                        <a href="/" className="fs-5">
+                          Model S
+                        </a>
                         {/* use this when model S page is completed */}
                         {/* <a href="/model-s">Model S</a> */}
                       </li>
                       <li>
-                        <a href="/">Model 3</a>
+                        <a href="/" className="fs-5">
+                          Model 3
+                        </a>
                       </li>
                       <li>
-                        <a href="/">Model X</a>
+                        <a href="/" className="fs-5">
+                          Model X
+                        </a>
                       </li>
                       <li>
-                        <a href="/">Model Y</a>
+                        <a href="/" className="fs-5">
+                          Model Y
+                        </a>
                       </li>
                       <li>
-                        <a href="/">Solar Roof</a>
+                        <a href="/" className="fs-5">
+                          Solar Roof
+                        </a>
                       </li>
                       <li>
-                        <a href="/">Solar Panels</a>
+                        <a href="/" className="fs-5">
+                          Solar Panels
+                        </a>
                       </li>
                     </ul>
                   </nav>
-                  <ul className="nav__buttons">
+                  <ul className="nav__buttons mt-3">
                     <li>
-                      <a className="cta" href="localhost:3000/">
+                      <a className="cta fs-5" href="localhost:3000/">
                         <button>Shop</button>
                       </a>
                     </li>
                     <li>
-                      <a className="cta" href="localhost:3000/">
+                      <a className="cta fs-5" href="localhost:3000/">
                         <button>Account</button>
                       </a>
                     </li>
                     <li>
-                      <a className="cta" href="localhost:3000/">
+                      <a className="cta fs-5" href="localhost:3000/">
                         <button>Menu</button>
                       </a>
                     </li>
@@ -69,8 +82,8 @@ class Home extends React.Component {
               <div className="section section1">
                 <div className="model-3-img">
                   <div className="title">
-                    <h2>Model 3</h2>
-                    <p>
+                    <h2 className="titleName">Model 3</h2>
+                    <p className="fs-3">
                       Order Online for <u>Touchless Delivery</u>
                     </p>
                   </div>
@@ -79,9 +92,9 @@ class Home extends React.Component {
                       //fix this to become my own build your own tesla page
                       href="https://www.tesla.com/model3/design#overview"
                     >
-                      <button className="button1">CUSTOM ORDER</button>
+                      <button className="button1 fs-4">Custom Order</button>
                     </a>
-                    <button className="button2">EXISTING INVENTORY</button>
+                    <button className="button2 fs-4">Existing Inventory</button>
                   </div>
                 </div>
               </div>
@@ -90,8 +103,8 @@ class Home extends React.Component {
               <div className="section section2">
                 <div className="model-y-img">
                   <div className="title">
-                    <h2>Model Y</h2>
-                    <p>
+                    <h2 className="titleName">Model Y</h2>
+                    <p className="fs-3">
                       Order Online for <u>Touchless Delivery</u>
                     </p>
                   </div>
@@ -100,9 +113,9 @@ class Home extends React.Component {
                       //fix this to become my own build your own tesla page
                       href="https://www.tesla.com/modelY/design#overview"
                     >
-                      <button className="button1">CUSTOM ORDER</button>
+                      <button className="button1 fs-4">Custom Order</button>
                     </a>
-                    <button className="button2">EXISTING INVENTORY</button>
+                    <button className="button2 fs-4">Existing Inventory</button>
                   </div>
                 </div>
               </div>
@@ -111,8 +124,8 @@ class Home extends React.Component {
               <div className="section section3">
                 <div className="model-s-img">
                   <div className="title">
-                    <h2>Model S</h2>
-                    <p>
+                    <h2 className="titleName">Model S</h2>
+                    <p className="fs-3">
                       Order Online for <u>Touchless Delivery</u>
                     </p>
                   </div>
@@ -121,9 +134,9 @@ class Home extends React.Component {
                       //fix this to become my own build your own tesla page
                       href="https://www.tesla.com/modelS/design#overview"
                     >
-                      <button className="button1">CUSTOM ORDER</button>
+                      <button className="button1 fs-4">Custom Order</button>
                     </a>
-                    <button className="button2">EXISTING INVENTORY</button>
+                    <button className="button2 fs-4">Existing Inventory</button>
                   </div>
                 </div>
               </div>
@@ -132,8 +145,8 @@ class Home extends React.Component {
               <div className="section section4">
                 <div className="model-x-img">
                   <div className="title">
-                    <h2>Model X</h2>
-                    <p>
+                    <h2 className="titleName">Model X</h2>
+                    <p className="fs-3">
                       Order Online for <u>Touchless Delivery</u>
                     </p>
                   </div>
@@ -142,9 +155,9 @@ class Home extends React.Component {
                       //fix this to become my own build your own tesla page
                       href="https://www.tesla.com/modelX/design#overview"
                     >
-                      <button className="button1">CUSTOM ORDER</button>
+                      <button className="button1 fs-4">Custom Order</button>
                     </a>
-                    <button className="button2">EXISTING INVENTORY</button>
+                    <button className="button2 fs-4">Existing Inventory</button>
                   </div>
                 </div>
               </div>
@@ -153,17 +166,17 @@ class Home extends React.Component {
               <div className="section section5">
                 <div className="solar-panels-img">
                   <div className="title">
-                    <h2>Solar Panels</h2>
-                    <p>Lowest Cost Solar Panels In America</p>
+                    <h2 className="titleName">Solar Panels</h2>
+                    <p className="fs-3">Lowest Cost Solar Panels In America</p>
                   </div>
                   <div className="order-buttons">
                     <a
                       //fix this to become my own build your own tesla page
                       href="https://www.tesla.com/energy/design"
                     >
-                      <button className="button1">ORDER NOW</button>
+                      <button className="button1 fs-4">Order Now</button>
                     </a>
-                    <button className="button2">LEARN MORE</button>
+                    <button className="button2 fs-4">Learn More</button>
                   </div>
                 </div>
               </div>
@@ -172,17 +185,17 @@ class Home extends React.Component {
               <div className="section section6">
                 <div className="solar-roof-img">
                   <div className="title">
-                    <h2>Solar Roof</h2>
-                    <p>Produce Clean Energy From Your Roof</p>
+                    <h2 className="titleName">Solar Roof</h2>
+                    <p className="fs-3">Produce Clean Energy From Your Roof</p>
                   </div>
                   <div className="order-buttons">
                     <a
                       //fix this to become my own build your own tesla page
                       href="https://www.tesla.com/energy/design"
                     >
-                      <button className="button1">ORDER NOW</button>
+                      <button className="button1 fs-4">Order Now</button>
                     </a>
-                    <button className="button2">LEARN MORE</button>
+                    <button className="button2 fs-4">Learn More</button>
                   </div>
                 </div>
               </div>
@@ -191,27 +204,27 @@ class Home extends React.Component {
               <div className="section">
                 <div className="accessories-img">
                   <div className="title">
-                    <h2>Accessories</h2>
+                    <h2 className="titleName">Accessories</h2>
                   </div>
-                  <div className="order-buttons">
+                  <div className="order-buttons pb-4">
                     <a
                       //fix this to become my own build your own tesla page
                       href="https://shop.tesla.com/?tesref=true"
                     >
-                      <button className="button1">SHOP NOW</button>
+                      <button className="button1 fs-4">SHOP NOW</button>
                     </a>
                   </div>
-                </div>
-                <div className="footer">
-                  <footer>
-                    <li>Tesla © 2022</li>
-                    <li>Privacy and Legal</li>
-                    <li>Contact</li>
-                    <li>Careers</li>
-                    <li>News</li>
-                    <li>Engage</li>
-                    <li>Locations</li>
-                  </footer>
+                  <div className="footer pt-3">
+                    <footer>
+                      <li>Tesla © 2022</li>
+                      <li>Privacy and Legal</li>
+                      <li>Contact</li>
+                      <li>Careers</li>
+                      <li>News</li>
+                      <li>Engage</li>
+                      <li>Locations</li>
+                    </footer>
+                  </div>
                 </div>
               </div>
             </div>
